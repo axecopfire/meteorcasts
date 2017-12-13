d7e7d45cac266711d5b35a4f787d29cf34181b83
axecopfire <axecopfire@users.noreply.github.com>
2017-12-13 12:29:51 -0500

    constructor method added

--- images/client/main.js
+++ images/client/main.js
@@ -10,6 +10,12 @@ import ImageList from './components/image_list';
 
 // Create a component
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= { images: [] };
+    }
+
     componentWillMount() {
         // Fantastic place to load data!
         axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
