3d506e652a0c03d54a07e5cc96407fac87bb8818
axecopfire <axecopfire@users.noreply.github.com>
2017-12-12 19:36:36 -0500

    image list component init

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,1 +1,14 @@
 // Create our image list component
+// Import React
+import React from 'react';
+
+// Create our component
+const ImageList = () => {
+    return (
+        <ul>
+            <li>Image Detail</li>
+        </ul>
+    );
+};
+
+// Export our component
