8f5c56bca29eba34e98dad8df5bc52769cf56176
axecopfire <axecopfire@users.noreply.github.com>
2017-12-12 22:55:42 -0500

    dummy images made

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -3,6 +3,12 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+const IMAGES = [
+    { title: 'Pen', link: 'https://dummyimage.com/600x400' },
+    { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+    { title: 'Mug', link: 'https://dummyimage.com/600x400'}
+];
+
 // Create our component
 const ImageList = () => {
     return (
