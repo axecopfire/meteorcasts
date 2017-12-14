41f1218a3541ad2fbfcd185ac4a1b68ba407c9fd
axecopfire <axecopfire@users.noreply.github.com>
2017-12-13 22:11:26 -0500

    boilerplate init for image_score

---
+++ images/client/components/image_score.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageScore = (props) => {
+  // props.ups is the number of up votes
+  // props.downs is the number of down votes
+
+};
+
+export default ImageScore;
