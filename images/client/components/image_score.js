f0d94bc7e49f9c3cac24cf7657a6826a5b3509d6
axecopfire <axecopfire@users.noreply.github.com>
2017-12-13 22:17:59 -0500

    image_score created and styled

--- images/client/components/image_score.js
+++ images/client/components/image_score.js
@@ -4,6 +4,20 @@ const ImageScore = (props) => {
   // props.ups is the number of up votes
   // props.downs is the number of down votes
 
+  const { ups, downs } = props;
+
+  const upsPercent = `${100 * (ups / (ups + downs))}%`;
+  const downsPercent = `${100 * (downs / (ups + downs))}%`;
+
+  return (
+    <div>
+      Ups/Downs
+      <div className='progress'>
+        <div style={{ width: upsPercent }} className="progress-bar progress-bar-success progress-bar-striped" />
+        <div style={{ width: downsPercent }} className='progress-bar progress-bar-danger progress-bar-striped' />
+      </div>
+    </div>
+  )
 };
 
 export default ImageScore;
