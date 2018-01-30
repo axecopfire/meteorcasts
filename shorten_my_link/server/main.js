d52128b4d3b7ea92447375f49f6b52f13c9e9e30
axecopfire <braun.sn11@gmail.com>
2018-01-30 11:51:24 -0500

    user redirect is successful

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -16,15 +16,20 @@ function onRoute(req, res, next) {
   // matching link in the Links collection
   const link = Links.findOne({ token: req.params.token });
 
+  if (link) {
   // If we find a link oject, redirect the suer to the
   // long URL
-
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+  } else {
   // If we don't find a link object, send the user
   // to our normal React app
+    next();
+  }
 }
 
 const middleware = ConnectRoute(function(router) {
-  router.get('/:token', (req) => console.log(req));
+  router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
