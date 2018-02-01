7bdb9c01b7b517b4a06fae5dea7a4c36e80616fa
axecopfire <braun.sn11@gmail.com>
2018-02-01 10:56:33 -0500

    bin share init

--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -3,7 +3,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
 import BinsViewer from './bins_viewer';
-
+import BinsShare from './bins_share';
 class BinsMain extends Component {
   render() {
     if(!this.props.bin) { return<div>Loading...</div>}
@@ -12,6 +12,7 @@ class BinsMain extends Component {
       <div>
         <BinsEditor bin={this.props.bin}/>
         <BinsViewer bin={this.props.bin} />
+        <BinsShare bin={this.props.bin}/>
       </div>
     );
   }
